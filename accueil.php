75181f66ab6c76792779445f6499b7d5065b4675
Eric Pham <pvesk77@gmail.com>
2020-05-05 11:03:19 +0200

    MAJ Accueil

---
+++ accueil.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>WallTech - Accueil</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/accueil.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <article class="col-md-12">
+                        <img src="logo.png" class="img-thumbnail" alt="Responsive image">
+                    </article>
+                    <article class="col-md-12">
+                        <form>
+                        <div class="form-group">
+                            <input type="text" id="login" class="form-control" name="login" placeholder="Nom d'utilisateur">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" id="mdp" class="form-control" name="mdp" placeholder="Mot de passe">
+                        </div>
+                            <input type="submit" class="fadeIn fourth" value="Se connecter">
+                        </form>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
