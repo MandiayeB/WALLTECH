fc5acbc028b3354267b11f56b3df8d4ac1092731
Allane Vong <vong@intechinfo.fr>
2020-05-15 15:38:14 +0200

    Déconnexion sur chat

--- chat.php
+++ chat.php
@@ -83,6 +83,12 @@
                             Cours
                             </a>
                         </li>
+                        <a class="nav-link" href="accueil.php">
+                            <!--<i class="fa fa-bell">
+                                <span class="badge badge-info">11</span>  NOTIFICATION
+                            </i>-->
+                            Déconnexion
+                            </a>
                     </ul>
                     <form class="form-inline my-2 my-lg-0">
                         <input class="form-control mr-sm-2" type="text" placeholder="Recherche" aria-label="Search">
